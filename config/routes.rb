9f345bc9d9d01639f82b1463ef21e0ec6cf1b5c1
Waihon Yew <yewwaihon@gmail.com>
2022-01-29 19:27:39 +0800

    Change path to about-us but retain the route name as about

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # GET /about
-  get "about", to: "about#index"
+  get "about-us", to: "about#index", as: :about
 
   root to: "main#index"
 end
