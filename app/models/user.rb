1de8924898b514cf8b2b02fda7f3f6830e46bbc8
Waihon Yew <yewwaihon@gmail.com>
2022-01-30 23:07:43 +0800

    Validate that email must be present and has a valid format

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
   # password:string virtual
   # password_confirmation:string virtual
   has_secure_password
+
+  validates :email, presence: true
+  validates :email, format: { with: /\A[^@\s]+@[^@\s]+\z/,
+                              message: "must be a valid mail address" }
 end
