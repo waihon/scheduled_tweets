369d5497aae6e814d1635108a57f367ffbb4383f
Waihon Yew <yewwaihon@gmail.com>
2022-01-30 23:04:42 +0800

    Enforce that email cannot be null at the database level

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2022_01_30_141506) do
 
   create_table "users", force: :cascade do |t|
-    t.string "email"
+    t.string "email", null: false
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
