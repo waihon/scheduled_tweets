0a5ca8e4ed9ad394d38c6e1397bcda86db228501
Waihon Yew <yewwaihon@gmail.com>
2022-02-17 00:03:41 +0800

    Publish to Twitter when publish_at has reached

--- app/jobs/tweet_job.rb
+++ app/jobs/tweet_job.rb
@@ -1,7 +1,19 @@
 class TweetJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform(tweet)
+    # Cater for tweets that have been brought forward in time
+    # 12 pm --> 8 am
+    # 8 am: should publish the tweet and set the tweet_id
+    # 12 pm: published, does nothing
+    return if tweet.published?
+
+    # Cater for tweets that have been rescheduled to the future
+    # 9 am --> 1 pm
+    # 9 am: publish_at not reached yet, does nothing
+    # 1 pm: should publish the tweet and set the tweet_id
+    return if tweet.publish_at > Time.current
+
+    tweet.publish_to_twitter!
   end
 end
